a323ad8095b029aa78aca67bbeb0ca172a69bbec
erick <erick_rodrigues@ufms.br>
2025-06-29 13:56:00 -0400

    Added sum, sub and multiplication methods

--- t2/Vector.java
+++ t2/Vector.java
@@ -10,38 +10,67 @@ import java.util.*;
 public final class Vector
   implements Iterable<VectorElementInfo>
 {
-  private final int n;
-  private final double v[];
+  private final int _n;
+  private final double[] _data;
   
   public Vector(int n)
   {
-    this.n = n;
-    v = new double[n];
-  }
+    this._n = n;
+    _data = new double[n];
+  } // Ctor
 
   public int size()
   {
-    return this.n;
-  }
+    return this._n;
+  } // size
 
   public double get(int i)
   {
-    return v[i];
-  }
+    return _data[i];
+  } // get
 
   public void set(int i, double s)
   {
-    v[i] = s;
-  }
-  
+    _data[i] = s;
+  } // set
+
+  public void sum(double s)
+  {
+    for (int i = 0; i < _n; i++)
+      _data[i] += s;
+  } // scalar sum
+
+  public void mul(double s)
+  {
+    for(int i = 0; i < _n; i++)
+      _data[i] *= s;
+  } // scalar mul
+
+  public void sub(double s)
+  {
+    sum((-1)*s);
+  } // scalar sub
+
+  public void sum(Vector v)
+  {
+    for (int i = 0; i < _n; i++)
+      _data[i] += v._data[i];
+  } // vector sum
+
+  // É o mesmo código da soma, mas evitei chamar a soma com -v para evitar
+  // junção posterior que é mais lento (deve afetar nada a performance, mas vou tentar deixar o mais performante possível
+  public void sub(Vector v)
+  {
+    for (int i = 0; i < _n; i++)
+      _data[i] -= v._data[i];
+  } // vector sub
+
   public void print()
   {
-    for(int i = 0; i < n; i++)
-    {
-      System.out.printf("%.2f ", v[i]);
-    }
+    for(double d : _data)
+      System.out.printf("%.2f ", d);
     System.out.println();
-  }
+  } //print
 
   @Override
   public Iterator<VectorElementInfo> iterator()
@@ -52,25 +81,25 @@ public final class Vector
   public class VectorIterator
           implements Iterator<VectorElementInfo>
   {
-    private final Vector V;
+    private final Vector v;
     private int i;
     
-    public VectorIterator(Vector V)
+    public VectorIterator(Vector v)
     {
-      this.V = V;
+      this.v = v;
       i = 0;
     }
     
     @Override
     public boolean hasNext()
     {
-      return i < V.size();
+      return i < v.size();
     }
     
     @Override
     public VectorElementInfo next()
     {
-      VectorElementInfo info = new VectorElementInfo(i, V.get(i));
+      VectorElementInfo info = new VectorElementInfo(i, v.get(i));
       i++;
       return info;
     }
