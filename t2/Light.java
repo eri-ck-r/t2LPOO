3d33de3a146a99a064cb8b4af1939e5674a68764
erick <erick_rodrigues@ufms.br>
2025-06-29 12:50:55 -0400

    Added GameObject constructor

--- t2/Light.java
+++ t2/Light.java
@@ -11,11 +11,14 @@ public class Light extends Component
   private GameConst type;
   private Color color;
   
+  public Light(Scene.GameObject o)
+  {
+    super(o);
+  }
   public Light(Scene.GameObject o, String type)
   {
     super(o);
-    
-    type = GameConst.
+    this.type = new GameConst(type, 1);
   }
   
   public void inspect()
