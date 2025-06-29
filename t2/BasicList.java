b1e98b7d3f3d69ba10264dcc89bc5005e42526c3
erick <erick_rodrigues@ufms.br>
2025-06-29 17:32:05 -0400

    Created BasicList

---
+++ t2/BasicList.java
@@ -0,0 +1,29 @@
+package t2;
+
+import java.util.ArrayList;
+
+public abstract class BasicList<T> extends ArrayList<T>
+{
+    public T getElement(String name)
+    {
+        for(int i = 0 ; i < this.size(); i++)
+        {
+            if(this.get(i).equals(name))
+                return this.get(i);
+        }
+        return null;
+    }
+
+    public boolean removeElement(String name)
+    {
+        for(int i = 0 ; i < this.size(); i++)
+        {
+            if(this.get(i).equals(name))
+            {
+                this.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
