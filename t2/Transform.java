b8695c42683c4ccafdac1f78d3d39d644999d327
erick <erick_rodrigues@ufms.br>
2025-06-29 17:36:39 -0400

    Added setScale method and Override of virtual method

--- t2/Transform.java
+++ t2/Transform.java
@@ -11,14 +11,19 @@ public class Transform extends Component
   private Vector position;
   private Vector rotation;
   private double scale;
-  
+
   public Transform(GameObject o)
   {
     super(o);
     position = new Vector(3);
     rotation = new Vector(3);
   }
-  
+
+  public void setScale(double s)
+  {
+      scale = s;
+  }
+
   public void inspect()
   {
 	  this.general_inspect();
@@ -28,5 +33,10 @@ public class Transform extends Component
 	  rotation.print();
 	  System.out.printf("Scale: %.2f", scale);
   }
-  
+
+  @Override
+  public void displayAttributes()
+  {
+      //TODO
+  }
 }  // Transform
