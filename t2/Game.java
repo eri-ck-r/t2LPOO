beeb674693a2d4d0a539ea6561169f7a15f1da5e
erick <erick_rodrigues@ufms.br>
2025-06-29 17:33:31 -0400

    Added Game's operations (addScene, removeScene/ , getScene and forEach)

--- t2/Game.java
+++ t2/Game.java
@@ -8,5 +8,31 @@ package t2;
 public class Game
 {
   private SceneList scenes;
-  
+
+  public Game()
+  {
+    scenes = new SceneList();
+
+  }
+
+  public void addScene(Scene s)
+  {
+    scenes.add(s);
+  }
+
+  public boolean removeScene(String name)
+  {
+    return scenes.removeElement(name);
+  }
+
+  public Scene getScene(String name)
+  {
+    return scenes.getElement(name);
+  }
+
+  public void forEach()
+  {
+//Todo
+
+  }
 }  // Game
