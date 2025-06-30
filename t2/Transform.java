c612a6e6f4148e11b60f34a2d42db657d5f996b7
erick <erick_rodrigues@ufms.br>
2025-06-29 20:12:10 -0400

    Added setters and getters for position, rotation and scale

--- t2/Transform.java
+++ t2/Transform.java
@@ -8,15 +8,20 @@ package t2;
 
 public class Transform extends Component
 {
-  private Vector position;
-  private Vector rotation;
+  private Vector3 position;
+  private Vector3 rotation;
   private double scale;
 
   public Transform(GameObject o)
   {
     super(o);
-    position = new Vector(3);
-    rotation = new Vector(3);
+    position = new Vector3();
+    rotation = new Vector3();
+  }
+
+  public double getScale()
+  {
+      return scale;
   }
 
   public void setScale(double s)
@@ -24,6 +29,25 @@ public class Transform extends Component
       scale = s;
   }
 
+  public Vector3 getPosition()
+  {
+      return position;
+  }
+
+  public void setPosition(Vector3 v)
+  {
+      position = v;
+  }
+
+  public Vector3 getRotation()
+  {
+      return rotation;
+  }
+
+  public void setRotation(Vector3 v)
+  {
+      rotation = v;
+  }
   public void inspect()
   {
 	  this.general_inspect();
