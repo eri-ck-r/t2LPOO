537669480e7cfb5ea3b4e3d0e9613f3025d33b78
eri-ck-r <erick_rodrigues@ufms.br>
2025-07-11 01:25:04 -0400

    Refactored to not be an abstract class

--- main/t2/BasicList.java
+++ main/t2/BasicList.java
@@ -8,7 +8,7 @@ package t2;
 
 import java.util.ArrayList;
 
-public abstract class BasicList<T extends NamedObject> extends ArrayList<T> 
+public class BasicList<T extends NamedObject> extends ArrayList<T> 
 {
     public T getElement(String name)
     {
